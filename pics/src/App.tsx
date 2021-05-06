4399d8239fc17a94d4ae1e436bbb37102fac28b2
himelr <rebelhaze@gmail.com>
2021-05-06 14:28:25 +0300

    Update to search bind

--- pics/src/App.tsx
+++ pics/src/App.tsx
@@ -3,12 +3,21 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 import 'semantic-ui-css/semantic.min.css'
 
-const App = () => {
-  return (
-    <div className="App ui container" style={{ marginTop: '10px' }}>
-		<SearchBar></SearchBar>
-    </div>
-  );
+class App extends React.Component {
+
+	onSearch(search: string) {
+		console.log(search + 'Kap');
+	}
+
+
+	render() {
+		return (
+			<div className="App ui container" style={{ marginTop: '10px' }}>
+				<SearchBar search={this.onSearch}></SearchBar>
+			</div>
+		  );
+	}
 }
+
 
 export default App;
--- pics/src/components/SearchBar.tsx
+++ pics/src/components/SearchBar.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-class SearchBar extends React.Component {
 
-	state = { term: '' }
+interface Props {
+	search: (search: string) => void;
+}
+class SearchBar extends React.Component<Props> {
 
-	onSubmit() {
-		console.log('Submit');
+	state = { term: 'Hi' }
+
+	onFormSubmit = (e: React.FormEvent<EventTarget>) => {
+		e.preventDefault();
+		this.props.search(this.state.term);
 	}
 
 	render() {
 		return (
 			<div className="ui segment">
-				<form className="ui form" onSubmit={this.onSubmit}>
+				<form className="ui form" onSubmit={this.onFormSubmit}>
 					<div className="field">
 						<label htmlFor="search">Image search</label>
 						<input id="search" type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })}></input>
